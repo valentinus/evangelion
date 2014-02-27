33167d9fc4526022ed81fe8cc20c7e9443edc489
Freddy <fkresna@gmail.com>
2014-02-27 10:14:40 +0700

    Create view & logic for Department Master

--- departmentMaster.php
+++ departmentMaster.php
@@ -2,4 +2,29 @@
 /**
  * User: Freddy
  * Date: 2/26/14
- */ 
+ */
+
+include_once "lib/twig.php";
+include "lib/konstanta.php";
+include "lib/db.php";
+
+$twig = new Twig();
+
+$konstanta = new Konstanta();
+$db = new Database();
+$dbh = $db->init();
+$departmentName = isset($_GET["departmentName"])?$_GET["departmentName"]:"";
+
+if ($departmentName != "") {
+    //TODO : Search if those name already input or not
+
+    $stmt = $dbh->prepare("INSERT INTO `department`(`name`)
+            VALUES (:departmentName)");
+
+    $stmt->bindParam(':departmentName',$departmentName);
+
+    $stat = $stmt->execute();
+    $twig->display("departmentMaster.html",array("stat"=>$stat));
+} else {
+    $twig->display("departmentMaster.html");
+}
